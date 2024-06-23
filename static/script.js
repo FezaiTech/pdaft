b9256f282141a1c0e8588643f27ab158bbb97072
zahidayturan <21060607@stu.omu.edu.tr>
2024-06-23 22:27:11 +0300

    add html and other js edits
    templates

--- static/script.js
+++ static/script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentSlide = 0;
     let logs = [];
     let resultMessage;
+
     const form = document.getElementById('expression-form');
     const resultSection = document.getElementById('result');
     const resultStage = document.querySelector('.result-stage');
@@ -28,16 +29,16 @@ document.addEventListener('DOMContentLoaded', function() {
             },
             body: JSON.stringify({ inputData: inputData })
         })
-        .then(response => response.json())
-        .then(data => {
-            logs = data.logs;
-            resultMessage = data.message;
-            resultSection.style.display = 'flex';
-            currentSlide = 0;
-            submitIcon.src = 'static/assets/refresh.png';
-            showSlides(logs);
-        })
-        .catch(error => console.error('Error:', error));
+            .then(response => response.json())
+            .then(data => {
+                logs = data.logs;
+                resultMessage = data.message;
+                resultSection.style.display = 'flex';
+                currentSlide = 0;
+                submitIcon.src = 'static/assets/refresh.png';
+                showSlides();
+            })
+            .catch(error => console.error('Error:', error));
     });
 
     submitButton.addEventListener('click', function(event) {
@@ -48,21 +49,21 @@ document.addEventListener('DOMContentLoaded', function() {
             resultSection.style.display = 'none';
             appText.style.display = 'flex';
             submitIcon.src = 'static/assets/start.png';
-            submitButton.style.background = stageTextColors['var(--green-color)'];
+            submitButton.style.background = stageTextColors['finish-stage-green'];
             updateTopInfo();
             form.submit();
         }
     });
 
-    function showSlides(logs) {
-        updateSlide(currentSlide);
+    function showSlides() {
+        updateSlide();
         updateButtonState();
         updateTopInfo();
     }
 
     function updateTopInfo() {
-        if (!logs || logs.length === 0) {
-            topStageText.innerHTML = 'İfade Bekleniyor';
+        if (!logs.length) {
+            topStageText.textContent = 'İfade Bekleniyor';
             topStageText.style.color = stageTextColors['ready-stage'];
             stageBoxes[1].style.background = "";
             stageBoxes[2].style.background = "";
@@ -70,13 +71,13 @@ document.addEventListener('DOMContentLoaded', function() {
             submitButton.style.background = stageTextColors['finish-stage-green'];
         } else if (currentSlide === logs.length - 1) {
             appText.style.display = 'none';
-            if(resultMessage === 'Geçerli ifade.'){
-                topStageText.innerHTML = 'İfade Kontrol Edildi. Geçerli';
+            if(resultMessage === 'Geçerli ifade.') {
+                topStageText.textContent = 'İfade Kontrol Edildi. Geçerli';
                 topStageText.style.color = stageTextColors['finish-stage-green'];
                 stageBoxes[2].style.background = stageTextColors['finish-stage-green'];
                 submitButton.style.background = stageTextColors['finish-stage-green'];
-            }else{
-                topStageText.innerHTML = 'İfade Kontrol Edildi. Geçersiz';
+            } else {
+                topStageText.textContent = 'İfade Kontrol Edildi. Geçersiz';
                 topStageText.style.color = stageTextColors['finish-stage-red'];
                 stageBoxes[2].style.background = stageTextColors['finish-stage-red'];
                 stageBoxes[2].style.borderColor = stageTextColors['finish-stage-red'];
@@ -84,28 +85,33 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         } else {
             appText.style.display = 'none';
-            topStageText.innerHTML = 'İfade Kontrol Ediliyor';
+            topStageText.textContent = 'İfade Kontrol Ediliyor';
+            topStageText.style.color = stageTextColors['run-stage'];
             stageBoxes[1].style.background = stageTextColors['run-stage'];
             stageBoxes[2].style.background = "";
             stageBoxes[2].style.borderColor = stageTextColors['finish-stage-green'];
             submitButton.style.background = stageTextColors['finish-stage-green'];
-            topStageText.style.color = stageTextColors['run-stage'];
         }
     }
 
-    function updateSlide(index) {
-        const log = logs[index];
+    function updateSlide() {
+        const log = logs[currentSlide];
         const stackElements = log.stack.split('').map(item => `<p>${item}</p>`).join('');
-        const prevLogState = index > 0 ? logs[index-1].state : "z";
+        const prevLogState = currentSlide > 0 ? logs[currentSlide-1].state : "z";
         let explanation;
-        if(log.state === 'z'){
-            explanation = "<p>İfadeniz hazırlandı<br>İşleme başlayabilirsiniz</p>";
-        }else if(log.state === 'f'){
-            explanation = "<p>Geçerli ifade<br>Simülasyon tamamlandı</p>";
-        }else if(log.state === 'qf'){
-            explanation = "<p>Geçersiz ifade<br>Simülasyon sonlandırıldı</p>";
-        }else{
-            explanation = "<p>Geçerli ifade olabilir<br>Sonraki aşamaya geçebilirsiniz</p>";
+
+        switch(log.state) {
+            case 'z':
+                explanation = "<p>İfadeniz hazırlandı<br>İşleme başlayabilirsiniz</p>";
+                break;
+            case 'f':
+                explanation = "<p>Geçerli ifade<br>Simülasyon tamamlandı</p>";
+                break;
+            case 'qf':
+                explanation = "<p>Geçersiz ifade<br>Simülasyon sonlandırıldı</p>";
+                break;
+            default:
+                explanation = "<p>Geçerli ifade olabilir<br>Sonraki aşamaya geçebilirsiniz</p>";
         }
         resultStage.innerHTML = `<p>Aşama ${log.stage}/${logs.length}</p><p>${logs.slice(0, log.stage).map(l => l.char).join('')}</p>${explanation}`;
         resultLog.innerHTML = `<p>${logs.slice(0, log.stage).map(l => l.log).join('<br>')}</p>`;
@@ -115,39 +121,38 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function updateButtonState() {
-            if (currentSlide === 0) {
-                prevStageButton.disabled = true;prevStageButton.classList.add('disabled-button');
-            } else {
-                prevStageButton.disabled = false;prevStageButton.classList.remove('disabled-button');
-            }
-            if (currentSlide === logs.length - 1) {
-                nextStageButton.disabled = true;nextStageButton.classList.add('disabled-button');
-            } else {
-                nextStageButton.disabled = false;nextStageButton.classList.remove('disabled-button');
-            }
+        prevStageButton.disabled = currentSlide === 0;
+        nextStageButton.disabled = currentSlide === logs.length - 1;
+        prevStageButton.classList.toggle('disabled-button', currentSlide === 0);
+        nextStageButton.classList.toggle('disabled-button', currentSlide === logs.length - 1);
     }
 
     prevStageButton.addEventListener('click', function() {
         if (currentSlide > 0) {
             currentSlide--;
-            updateSlide(currentSlide);updateButtonState();updateTopInfo();
+            updateSlide();
+            updateButtonState();
+            updateTopInfo();
         }
     });
 
     nextStageButton.addEventListener('click', function() {
         if (currentSlide < logs.length - 1) {
             currentSlide++;
-            updateSlide(currentSlide);updateButtonState();updateTopInfo();
+            updateSlide();
+            updateButtonState();
+            updateTopInfo();
         }
     });
 
-    updateTopInfo();updateButtonState();
+    updateTopInfo();
+    updateButtonState();
 });
 
 document.getElementById('theme-toggle').addEventListener('click', function() {
     const currentTheme = document.documentElement.getAttribute('theme');
-    const appLogo = document.querySelector('.app-logo');
-    if (currentTheme === 'light') {document.documentElement.setAttribute('theme', 'dark');appLogo.src = 'static/assets/pdaft_light.png';
-    } else {document.documentElement.setAttribute('theme', 'light');appLogo.src = 'static/assets/pdaft_dark.png';
-    }
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    const appLogoSrc = newTheme === 'dark' ? 'static/assets/pdaft_light.png' : 'static/assets/pdaft_dark.png';
+    document.documentElement.setAttribute('theme', newTheme);
+    document.querySelector('.app-logo').src = appLogoSrc;
 });
